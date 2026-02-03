6af1e75625dd546838176cb80fa97cb811a35697
GitHub Action <action@github.com>
2026-02-03 05:15:09 +0000

    chore: update wiki config [skip ci]

--- wiki/config.js
+++ wiki/config.js
@@ -12,8 +12,8 @@ const CONFIG = {
     // Versioning Settings
     versioning: {
         type: "local",
-        manualVersion: "v1.0.0",
-        manualDate: "2026-02-02"
+        manualVersion: "v1.2.0",
+        manualDate: "2026-02-03"
     },
 
     // GitHub Repository
